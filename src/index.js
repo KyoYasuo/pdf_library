266b4bc753eca1bd20c4f9999a7dca6db27b85f3
KyoYasuo <kyoyasuo221@gmail.com>
2023-12-09 11:55:09 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,29 +1,3 @@
-import { renderPDF } from './rendering';
-import { navigatePages } from './navigation';
-import { zoomIn, zoomOut } from './zoom';
-import { highlightText } from './highlight';
-import { addComment } from './comment';
-import { insertPage, deletePage, reorderPages } from './pageManipulation';
-import { trackChanges, versionControl } from './versionControl';
+import rendering from './rendering';
 
-// Load the PDF document and render the initial page
-const loadPDF = async (url) => {
-    // Load the PDF document using pdf.js
-    const pdf = await pdfjsLib.getDocument(url).promise;
-
-    // Get the total number of pages in the PDF document
-    const numPages = pdf.numPages;
-
-    // Store the current page number
-    let currentPage = 1;
-
-    // Render the initial page
-    renderPage(pdf, currentPage);
-
-    // Add event listeners to previous and next buttons
-    const prevButton = document.getElementById('prev-button');
-    prevButton.addEventListener('click', () => goToPreviousPage(pdf, currentPage));
-
-    const nextButton = document.getElementById('next-button');
-    nextButton.addEventListener('click', () => goToNextPage(pdf, currentPage));
-};
+rendering.init();
--- src/rendering.js
+++ src/rendering.js
@@ -1,16 +1,71 @@
-export const renderPage = async (pdf, pageNumber) => {
-    const page = await pdf.getPage(pageNumber);
+import { PDFDocumentProxy } from 'pdfjs-dist';
 
-    const container = document.getElementById('pdf-container');
-    const canvas = document.createElement('canvas');
-    container.appendChild(canvas);
+const rendering = {
+    pdfDoc: null,
+    currentPage: 1,
 
-    const viewport = page.getViewport({ scale: 1.0 });
-    canvas.width = viewport.width;
-    canvas.height = viewport.height;
+    async init() {
+        // Load the PDF document
+        const pdfUrl = './assets/pdf/next-handbook.pdf';
+        this.pdfDoc = await PDFJS.getDocument(pdfUrl);
 
-    page.render({
-        canvasContext: canvas.getContext('2d'),
-        viewport: viewport,
-    });
+        // Render the initial page
+        this.renderPage(this.currentPage);
+
+        // Set up event listeners for navigation
+        const prevButton = document.getElementById('prev-button');
+        const nextButton = document.getElementById('next-button');
+        const goButton = document.getElementById('go-button');
+        prevButton.addEventListener('click', this.showPrevPage.bind(this));
+        nextButton.addEventListener('click', this.showNextPage.bind(this));
+        goButton.addEventListener('click', this.goToPage.bind(this));
+    },
+
+    async renderPage(pageNumber) {
+        const pageContainer = document.getElementById('pdf-container');
+        const page = await this.pdfDoc.getPage(pageNumber);
+
+        // Configure the viewport for rendering
+        const scale = 1.5;
+        const viewport = page.getViewport({ scale });
+
+        // Create a canvas element
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+        pageContainer.appendChild(canvas);
+
+        // Render the page content on the canvas
+        const renderContext = {
+            canvasContext: context,
+            viewport,
+        };
+        await page.render(renderContext);
+    },
+
+    async showPrevPage() {
+        if (this.currentPage > 1) {
+            this.currentPage--;
+            await this.renderPage(this.currentPage);
+        }
+    },
+
+    async showNextPage() {
+        if (this.currentPage < this.pdfDoc.numPages) {
+            this.currentPage++;
+            await this.renderPage(this.currentPage);
+        }
+    },
+
+    async goToPage() {
+        const pageInput = document.getElementById('page-input');
+        const pageNumber = parseInt(pageInput.value, 10);
+        if (pageNumber >= 1 && pageNumber <= this.pdfDoc.numPages) {
+            this.currentPage = pageNumber;
+            await this.renderPage(this.currentPage);
+        }
+    },
 };
+
+export default rendering;
