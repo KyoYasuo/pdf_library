3adb9d508874e37f1077712f52a4652f2b446345
KyoYasuo <kyoyasuo221@gmail.com>
2023-12-09 11:30:02 +0900

    update

--- src/rendering.js
+++ src/rendering.js
@@ -1,3 +1,16 @@
-export function renderPDF(pdfUrl, container) {
-    // Code for rendering the PDF document
-}
+export const renderPage = async (pdf, pageNumber) => {
+    const page = await pdf.getPage(pageNumber);
+
+    const container = document.getElementById('pdf-container');
+    const canvas = document.createElement('canvas');
+    container.appendChild(canvas);
+
+    const viewport = page.getViewport({ scale: 1.0 });
+    canvas.width = viewport.width;
+    canvas.height = viewport.height;
+
+    page.render({
+        canvasContext: canvas.getContext('2d'),
+        viewport: viewport,
+    });
+};
