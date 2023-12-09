0be41c92e01e91b27a73a83a9ed169a37cf4e497
KyoYasuo <kyoyasuo221@gmail.com>
2023-12-09 14:08:37 +0900

    update

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            'pdfjs-dist/build/pdf': 'pdfjs-dist/webpack',
+            'pdfjs-dist/build/pdf.worker': 'pdfjs-dist/webpack/pdf.worker.entry',
+        },
+    },
+};
